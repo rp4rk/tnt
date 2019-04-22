aa8a0921e2da9ed645120876c959f8c70a04b83e
Ryan Park <admin@ryanpark.co.uk>
2019-04-22 23:37:23 +0100

    Selector for date range

--- src/selectors/entries.js
+++ src/selectors/entries.js
@@ -1,4 +1,5 @@
 import * as FromEntries from "../reducers/entries";
+import { parse, eachDay } from "date-fns";
 
 const STATE_KEY = "entries";
 
@@ -13,3 +14,19 @@ export const getEntryProperty = (state, projectId, entryId, propertyId) =>
     entryId,
     propertyId
   );
+
+// Get days for an entry
+export const getEntryDays = (state, projectId, entryId) => {
+  const fromDate = getEntryProperty(state, projectId, entryId, "fromDate");
+  const toDate = getEntryProperty(state, projectId, entryId, "toDate");
+
+  if (!fromDate) {
+    return null;
+  }
+
+  if (!toDate) {
+    return parse(fromDate);
+  }
+
+  return eachDay(parse(fromDate), parse(toDate));
+};
